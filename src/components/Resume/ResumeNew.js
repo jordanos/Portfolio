7445ac39e58899643c95197c0abfca7fc951ac69
jordanos <y.dereje@yahoo.com>
2023-01-31 10:11:43 +0300

    resume updated

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,17 +2,18 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Soumyajit_Behera-BIT_MESRA.pdf";
+import pdf from "../../Assets/../Assets/Yordanos_resume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/soumyajit4419/portfolio/master/src/Assets/Yordanos_resume.pdf";
+  "https://raw.githubusercontent.com/jordanos/portfolio/master/src/Assets/Yordanos_resume.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
+  const [numPages, setNumPages] = useState(2);
 
   useEffect(() => {
     setWidth(window.innerWidth);
@@ -35,8 +36,15 @@ function ResumeNew() {
         </Row>
 
         <Row className="resume">
-          <Document file={resumeLink} className="d-flex justify-content-center">
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
+          <Document
+            file={resumeLink}
+            className="d-flex flex-column align-items-center"
+          >
+            {Array.apply(null, Array(numPages))
+              .map((x, i) => i + 1)
+              .map((page) => (
+                <Page pageNumber={page} scale={width > 786 ? 1.7 : 0.6} />
+              ))}
           </Document>
         </Row>
 
